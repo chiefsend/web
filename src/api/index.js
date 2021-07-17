b5f9f04c33952d866e2112803bc599bfe5ecb42c
Lukas <boehm-lukas@outlook.de>
2021-07-17 04:43:17 +0200

    :heavy_plus_sign:  add axios for api

---
+++ src/api/index.js
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const ax = axios.create({
+  baseURL: process.env.API || "http://localhost:6969",
+  headers: {
+    "Content-type": "application/json"
+  }
+});
+
+export default ax;
--- src/views/Public.vue
+++ src/views/Public.vue
@@ -1,9 +1,37 @@
 <template>
-  <p>Public</p>
+  <v-container>
+    <v-card v-for="sh in shares" :key="sh.id">
+      <v-card-title>
+        {{ sh.name }}
+      </v-card-title>
+      <v-card-subtitle>
+        Listen to your favorite artists and albums whenever and wherever, online
+        and offline.
+      </v-card-subtitle>
+      <v-card-actions>
+        <v-btn>
+          Open
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
 </template>
 
 <script>
+import ax from "@/api";
+
 export default {
-  name: "Public"
+  name: "Public",
+  data() {
+    return {
+      shares: []
+    };
+  },
+  created() {
+    ax.get("/shares").then(res => {
+      this.shares = res.data;
+      console.log(res.data);
+    });
+  }
 };
 </script>
