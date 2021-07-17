01fcd92c56fec24a00b121bd8a93947b934c839f
Lukas <boehm-lukas@outlook.de>
2021-07-17 04:43:29 +0200

    :lipstick:  basic stepper design

---
+++ src/components/UploadStep.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-card>
+    Hallo
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "UploadStep"
+};
+</script>
--- src/views/Upload.vue
+++ src/views/Upload.vue
@@ -1,9 +1,45 @@
 <template>
-  <p>Upload</p>
+  <v-container>
+    <p>Upload</p>
+
+    <v-stepper>
+      <v-stepper-header>
+        <v-stepper-step step="1">
+          Select
+        </v-stepper-step>
+
+        <v-divider></v-divider>
+
+        <v-stepper-step step="2">
+          Upload
+        </v-stepper-step>
+
+        <v-divider></v-divider>
+
+        <v-stepper-step step="3">
+          Share
+        </v-stepper-step>
+      </v-stepper-header>
+
+      <v-stepper-items>
+        <v-stepper-content step="1"><SelectStep /></v-stepper-content>
+        <v-stepper-content step="2"><UploadStep /></v-stepper-content>
+        <v-stepper-content step="3"><ShareStep /></v-stepper-content>
+      </v-stepper-items>
+    </v-stepper>
+  </v-container>
 </template>
 
 <script>
+import SelectStep from "@/components/SelectStep";
+import UploadStep from "@/components/UploadStep";
+import ShareStep from "@/components/ShareStep";
+
 export default {
-  name: "Upload"
+  name: "Upload",
+  components: { SelectStep, UploadStep, ShareStep },
+  data() {
+    return {};
+  }
 };
 </script>
