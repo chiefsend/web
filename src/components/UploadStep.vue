b0a0ea5bd2cc6fa74db6e0a532f1a0354fad286c
Lukas <boehm-lukas@outlook.de>
2021-07-18 04:53:09 +0200

    :recycle: clean up upload

--- src/components/UploadStep.vue
+++ src/components/UploadStep.vue
@@ -5,62 +5,28 @@
 </template>
 
 <script>
-// import ax from "@/api";
+import ax from "@/api";
 
 export default {
   name: "UploadStep",
   methods: {
     uploadAll(share, files) {
-      console.log(share);
-      console.log(files);
-
+      let reqs = [];
       for (let i = 0; i < files.length; i++) {
         var formdata = new FormData();
         formdata.append("file", files[i]);
-        var requestOptions = {
-          method: "POST",
-          body: formdata,
-          redirect: "follow"
-        };
-        fetch(
-          `http://localhost:6969/share/${share.id}/attachments`,
-          requestOptions
-        )
-          .then(res => console.log(res))
-          .catch(error => console.log("error", error));
+        reqs.push(ax.post(`/share/${share.id}/attachments`, formdata)); // TODO maybe need multipart/form-data header?
       }
 
-      // let reqs = [];
-      // for (let file in files) {
-      //   // let form = new FormData();
-      //   // form.append("file", file);
-      //   // reqs.push(ax.post(`/share/${share.id}/attachments`, form));
-
-      //   // var formdata = new FormData();
-      //   // formdata.append("file", file);
-      //   // var requestOptions = {
-      //   //   method: "POST",
-      //   //   body: formdata,
-      //   //   redirect: "follow"
-      //   // };
-      //   // fetch(
-      //   //   `http://localhost:6969/share/${share.id}/attachments`,
-      //   //   requestOptions
-      //   // )
-      //   //   .then(response => response.text())
-      //   //   .then(result => console.log(result))
-      //   //   .catch(error => console.log("error", error));
-      // }
-
-      // Promise.all(reqs)
-      //   .then(function(results) {
-      //     console.log("uploaded all attachments");
-      //     console.log(results);
-      //     this.$emit("done");
-      //   })
-      //   .catch(error => {
-      //     console.error(error);
-      //   });
+      Promise.all(reqs)
+        .then(function(results) {
+          console.log("uploaded all attachments");
+          console.log(results);
+          // this.$emit("done");
+        })
+        .catch(error => {
+          console.error(error);
+        });
     }
   }
 };
