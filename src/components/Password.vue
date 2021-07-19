85d7536685623d4f523f9ddbe45d93578e847387
Lukas <boehm-lukas@outlook.de>
2021-07-19 21:01:14 +0200

    :passport_control: :construction:  work on password protected download

---
+++ src/components/Password.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-bottom-sheet persistent inset v-model="sheet">
+    <v-card>
+      <v-alert>Password required</v-alert>
+      <v-text-field
+        :append-icon="show_password ? 'mdi-eye' : 'mdi-eye-off'"
+        :type="show_password ? 'text' : 'password'"
+        name="input-10-2"
+        @click:append="show_password = !show_password"
+      ></v-text-field>
+
+      <v-btn color="secondary" @click="$emit('close')">Cancel</v-btn>
+      <v-btn color="primary" @click="$emit('password', password)">Submit</v-btn>
+    </v-card>
+  </v-bottom-sheet>
+</template>
+
+<script>
+export default {
+  name: "Password",
+  props: {
+    sheet: Boolean,
+    id: String
+  },
+  data() {
+    return {
+      show_password: false,
+      password: ""
+    };
+  }
+};
+</script>
--- src/views/Download.vue
+++ src/views/Download.vue
@@ -8,7 +8,9 @@
         <v-list>
           <v-list-item v-for="file of share.files" :key="file.id">
             <v-list-item-title>{{ file.filename }}</v-list-item-title>
-            <v-list-item-subtitle>{{ file.filesize | filesize }}</v-list-item-subtitle>
+            <v-list-item-subtitle>
+              {{ file.filesize | filesize }}
+            </v-list-item-subtitle>
             <v-list-item-action>
               <v-btn
                 outlined
@@ -29,18 +31,29 @@
         </v-btn>
       </v-card-text>
     </v-card>
+
+    <Password
+      :sheet="sheet"
+      :id="share.id"
+      @close="sheet = false"
+      @password="handlePassword"
+    />
   </v-container>
 </template>
 
 <script>
 import ax from "@/api";
 import fileDownload from "js-file-download";
+import Password from "../components/Password.vue";
 
 export default {
   name: "Download",
+  components: { Password },
   data() {
     return {
-      share: {}
+      share: {},
+      sheet: false,
+      password: ""
     };
   },
   mounted() {
@@ -48,22 +61,38 @@ export default {
       .then(res => {
         this.share = res.data;
       })
-      .catch(error => {
-        console.log(error);
+      .catch(() => {
+        this.sheet = true;
       });
   },
   methods: {
     download(url) {
       ax.get(url, {
-        responseType: "blob"
-        // auth: {
-        //   username: this.id,
-        //   password: this.password
-        // }
+        responseType: "blob",
+        auth: {
+          username: this.id,
+          password: this.password
+        }
       }).then(res => {
         let fileName = res.headers["content-disposition"].split("filename=")[1];
         fileDownload(res.data, fileName);
       });
+    },
+    handlePassword(value) {
+      this.password = value;
+
+      ax.get(`/share/${this.$route.params.id}`, {
+        auth: {
+          username: this.id,
+          password: value
+        }
+      })
+        .then(res => {
+          console.log(res.data);
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
   }
 };
