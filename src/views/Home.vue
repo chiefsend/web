0e3419fb58f0b2f5a06ec1d7fa442b0ea3032a10
Lukas <boehm-lukas@outlook.de>
2021-07-24 20:12:48 +0200

    :lipstick: :art: actual home Page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,28 @@
 <template>
-  <v-parallax
-    src="https://cdn.vuetifyjs.com/images/parallax/material.jpg"
-  ></v-parallax>
+  <v-parallax src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg">
+    <v-row align="center" justify="center">
+      <v-col class="text-center" cols="12">
+        <h1 class="text-h4 font-weight-thin mb-4">
+          ChiefSend
+        </h1>
+        <h4 class="subheading mb-8">
+          Upload your files!
+        </h4>
+        <v-btn color="secondary" to="/public" class="mx-2">
+          public
+          <v-icon right>
+            mdi-earth
+          </v-icon>
+        </v-btn>
+        <v-btn color="success" to="/upload" class="mx-2">
+          Upload
+          <v-icon right>
+            mdi-cloud-upload
+          </v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+  </v-parallax>
 </template>
 
 <script>
