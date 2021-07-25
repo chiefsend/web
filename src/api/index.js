9ef075b6729387b7242e0a79a5a513b8a6f350bf
Lukas <boehm-lukas@outlook.de>
2021-07-25 23:24:45 +0200

    :bug: fix base url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const ax = axios.create({
-  baseURL: process.env.API || "http://localhost:6969",
+  baseURL:
+    process.env.NODE_ENV === "development"
+      ? "http://localhost:6969"
+      : window.location.origin + "/api",
   headers: {
     "Content-type": "application/json"
   }
