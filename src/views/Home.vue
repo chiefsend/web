459b6bcf712ab6ce31b33db167f21eaecca9278c
Lukas <boehm-lukas@outlook.de>
2021-07-26 16:01:05 +0200

    :sparkles: simple search feature on homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,31 +1,59 @@
 <template>
   <v-row align="center" justify="center" class="cover white--text">
-    <v-col class="text-center" cols="12">
+    <v-col class="text-center cover-content" cols="12">
       <h1 class="text-h4 font-weight-thin mb-4">
         ChiefSend
       </h1>
+
       <h4 class="subheading mb-8">
         Upload your files!
       </h4>
-      <v-btn color="secondary" to="/public" class="mx-2">
+
+      <v-btn block color="secondary" to="/public" class="ma-2">
         public
         <v-icon right>
           mdi-earth
         </v-icon>
       </v-btn>
-      <v-btn color="success" to="/upload" class="mx-2">
+
+      <v-btn block color="success" to="/upload" class="ma-2">
         Upload
         <v-icon right>
           mdi-cloud-upload
         </v-icon>
       </v-btn>
+
+      <h6 class="my-4">OR</h6>
+
+      <v-form @submit="search()" onSubmit="return false;">
+        <v-text-field
+          v-model="query"
+          label="Search for a Share"
+          outlined
+          dense
+        ></v-text-field>
+      </v-form>
     </v-col>
   </v-row>
 </template>
 
 <script>
 export default {
-  name: "Home"
+  name: "Home",
+  data() {
+    return {
+      query: ""
+    };
+  },
+  methods: {
+    search() {
+      if (this.query.length <= 0) return;
+
+      const split = this.query.split("/");
+      let id = split[split.length - 1];
+      this.$router.push({ name: "Download", params: { id: id } });
+    }
+  }
 };
 </script>
 
@@ -37,5 +65,9 @@ export default {
   background-position: center; /* Center the image */
   background-repeat: no-repeat; /* Do not repeat the image */
   background-size: cover; /* Resize the background image to cover the entire container */
+}
+
+.cover-content {
+  max-width: 400px;
 }
 </style>
