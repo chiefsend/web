b2f3cc5baa13902281a5430d61135eb8e718b9a9
Lukas <boehm-lukas@outlook.de>
2021-07-26 16:19:12 +0200

    :children_crossing: improve usability
    by adding a form with autofocus,
    submit on enter OR button press

--- src/components/Password.vue
+++ src/components/Password.vue
@@ -3,29 +3,28 @@
     <v-card>
       <v-card-text class="pt-4">
         <v-alert dense type="error" v-if="wrongPassword"
-          >wrong Password!</v-alert
+          >Wrong Password!</v-alert
         >
         <v-alert dense type="error" v-else>Password required!</v-alert>
 
-        <v-text-field
-          v-model="password"
-          :append-icon="show_password ? 'mdi-eye' : 'mdi-eye-off'"
-          :type="show_password ? 'text' : 'password'"
-          @click:append="show_password = !show_password"
-        ></v-text-field>
+        <v-form @submit="submit()" onSubmit="return false;">
+          <v-text-field
+            autofocus
+            v-model="password"
+            :append-icon="show_password ? 'mdi-eye' : 'mdi-eye-off'"
+            :type="show_password ? 'text' : 'password'"
+            @click:append="show_password = !show_password"
+          ></v-text-field>
 
-        <v-row>
-          <v-col>
-            <v-btn block color="secondary" @click="$emit('close')"
-              >Cancel</v-btn
-            >
-          </v-col>
-          <v-col>
-            <v-btn block color="primary" @click="$emit('password', password)"
-              >Submit</v-btn
-            >
-          </v-col>
-        </v-row>
+          <v-row>
+            <v-col>
+              <v-btn block color="secondary" @click="close()">Cancel</v-btn>
+            </v-col>
+            <v-col>
+              <v-btn block color="primary" @click="submit()">Submit</v-btn>
+            </v-col>
+          </v-row>
+        </v-form>
       </v-card-text>
     </v-card>
   </v-dialog>
@@ -44,6 +43,14 @@ export default {
       show_password: false,
       password: ""
     };
+  },
+  methods: {
+    close() {
+      this.$emit("close");
+    },
+    submit() {
+      this.$emit("password", this.password);
+    }
   }
 };
 </script>
